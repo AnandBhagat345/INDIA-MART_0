c334f4eccfd23977656f7f92742d04027bfcbe9c
AnandBhagat345 <anandbhagat345@.com>
2026-06-04 10:31:45 +0530

    again fixing

--- shop/views.py
+++ shop/views.py
@@ -6,7 +6,7 @@ from math import ceil
 import json
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-from paytm_gateway import checksum
+from mac.paytm_gateway import checksum
 
 
 # Create your views here.
